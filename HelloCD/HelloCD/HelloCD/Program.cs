c2a584ca6cb4d925893f82902b1035386eacc59e
rochak111 <91822867+rochak111@users.noreply.github.com>
2024-05-24 19:18:08 +0545

    Commit test

--- HelloCD/HelloCD/HelloCD/Program.cs
+++ HelloCD/HelloCD/HelloCD/Program.cs
@@ -8,20 +8,15 @@ namespace HelloCD
 {
     class Program
     {
-        private static int majorVersion = 1;
-        private static int minorVersion = 5;
+        private static int majorVersion = 2;
+        private static int minorVersion = 0;
         static void Main(string[] args)
         {
             string versionNumber = majorVersion.ToString() + "." + minorVersion.ToString();
             string message = "Hello CD! This program is version number";
             Console.WriteLine(message + " " + versionNumber);
-
-			Console.WriteLine("Now Rochak is  changing even more to really notice if it is a new verison.");
-	    Console.WriteLine("Now Rochak--2 is  changing even more to really notice if it is a new verison. ");
-	    Console.WriteLine("Now Rochak--5 is  changing even more to really notice if it is a new verison. ");
-		Console.WriteLine("Task completed----Done");
-
-            Console.WriteLine("Recheck-Press any key to exit.");
+			Console.WriteLine("Its the future!");
+            Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
         }
     }
